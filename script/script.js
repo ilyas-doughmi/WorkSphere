259f6e3e8db196acac702ab7b977d850b91d3379
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-15 12:10:02 +0100

    feat: adding script.js and setuping rooms to variables

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const salle_de_confirence = document.querySelector(".room1");
+const salle_de_server = document.querySelector(".room2");
+const salle_de_securite = document.querySelector(".room3");
+const salle_de_reception = document.querySelector(".room4");
+const salle_de_personnel = document.querySelector(".room5");
+const salle_archive = document.querySelector(".room6");
