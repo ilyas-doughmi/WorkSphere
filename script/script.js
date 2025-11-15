e8bbbb95c3ef096668d60e511be3aa6877d942c6
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-15 12:14:19 +0100

    feat: adding mouseover event listener to salle_de_confirence

--- script/script.js
+++ script/script.js
@@ -4,3 +4,7 @@ const salle_de_securite = document.querySelector(".room3");
 const salle_de_reception = document.querySelector(".room4");
 const salle_de_personnel = document.querySelector(".room5");
 const salle_archive = document.querySelector(".room6");
+
+salle_de_confirence.addEventListener("mouseover",function(){
+    console.log("confirence");
+})
