663967530a031ad6c4fb3e19d46b03740b0bb976
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-15 13:49:14 +0100

    feat: adding ids to room to identify them

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-const salle_de_confirence = document.querySelector(".room1");
-const salle_de_server = document.querySelector(".room2");
-const salle_de_securite = document.querySelector(".room3");
-const salle_de_reception = document.querySelector(".room4");
-const salle_de_personnel = document.querySelector(".room5");
-const salle_archive = document.querySelector(".room6");
+const salle_de_confirence = document.querySelector("#room1");
+const salle_de_server = document.querySelector("#room2");
+const salle_de_securite = document.querySelector("#room3");
+const salle_de_reception = document.querySelector("#room4");
+const salle_de_personnel = document.querySelector("#room5");
+const salle_archive = document.querySelector("#room6");
 
 salle_de_confirence.addEventListener("mouseover",function(){
     console.log("confirence");
