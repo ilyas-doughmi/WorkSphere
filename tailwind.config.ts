8e21a218a4cc713d69eafd70f8a33d6e98b2f8e2
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-18 15:07:42 +0100

    feat: changing tailwindcss from cdn to install

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./index.html", "./script/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
+
