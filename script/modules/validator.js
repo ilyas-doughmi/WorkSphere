b5c2822d6fa2e9bf5684590f9b8341294b8a6a03
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-18 14:45:01 +0100

    feat: adding to role select

--- script/modules/validator.js
+++ script/modules/validator.js
@@ -1,6 +1,10 @@
-export function validator(object){
-      if (object.fullname.length < 3) {
+export function validator(object) {
+    if (object.fullname.length < 3) {
         errors.name = "Name is too short (min 3 letters)";
         return false;
     }
+    if (object.role == "") {
+        errors.name = "Please select a job role"
+    }
+
 }
