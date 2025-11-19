ca28000e19c35fdfc209ffdd9c38d9e05f0ce61b
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-19 09:48:34 +0100

    feat: adding fully validator

--- script/modules/validator.js
+++ script/modules/validator.js
@@ -1,20 +1,34 @@
 export function validator(object) {
+
     if (object.fullname.length < 3) {
         errors.name = "Name is too short (min 3 letters)";
-        console.log(errors.name);
         return false;
     }
-    if (object.role == "") {
-        errors.name = "Please select a job role"
-        console.log(errors.name);
+
+    if (object.role === "") {
+        errors.name = "Please select a job role";
+        return false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(object.email)) {
+        errors.email = "Invalid email format";
+        return false;
+    }
+
+    const phoneRegex = /^06\d{8}$/;
+    if (!phoneRegex.test(object.phone)) {
+        errors.phone = "Phone must start with 06 and contain 10 digits";
         return false;
     }
 
     return true;
 }
 
-export function validate_exp(exp){
-     if (exp.title.length < 2) {
+
+export function validate_exp(exp) {
+
+    if (exp.title.length < 2) {
         errors.title = "Job Title is too short";
         return false;
     }
@@ -33,6 +47,7 @@ export function validate_exp(exp){
         errors.dates = "End date cannot be before Start date";
         return false;
     }
+
     return true;
 }
 
